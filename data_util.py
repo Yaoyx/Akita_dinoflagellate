50a8fb6abd0512fea1332995cf150fa325dd7a11
Yao Xiao <yxiao977@endeavour2.hpc.usc.edu>
2023-10-11 11:39:23 -0700

    update test_funcs

--- data_util.py
+++ data_util.py
@@ -2,14 +2,18 @@ import numpy as np
 import pandas as pd
 import cooltools
 import cooler
-import logging
 import functools
-logger = logging.getLogger('one')
+from multiprocessing import Pool
+
+import logging
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+logger = logging.getLogger('data_util')
 logger.setLevel(logging.DEBUG)
+logger.propagate = False  # Disable propagation to the root logger
+
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
@@ -32,11 +36,12 @@ def _bins_cis_total_ratio_filter(clr, thres):
     """
     coverage = cooltools.coverage(clr)
     cis_total_cov = coverage[0] / coverage[1]
-    bins_mask = cis_total_cov <= thres
+    bins_mask = cis_total_cov > thres
 
     return bins_mask
 
-def _pixels_filter(bins_table, chunk_pixels, bin_mask):
+### double check if filter work correctly
+def _pixels_filter(chunk_pixels, bins_table_size, bins_mask):
     """
     Filter out pixels that belong to bad bins.
 
@@ -46,7 +51,7 @@ def _pixels_filter(bins_table, chunk_pixels, bin_mask):
         A 2D array of shape (n_bins, n_features) containing the features of each bin.
     chunk_pixels : pandas.DataFrame
         A DataFrame containing the pixels to filter.
-    bin_mask : numpy.ndarray
+    bins_mask : numpy.ndarray
         A boolean array of shape (n_bins,) indicating which bins are bad.
 
     Returns:
@@ -54,19 +59,19 @@ def _pixels_filter(bins_table, chunk_pixels, bin_mask):
     pandas.DataFrame
         A DataFrame containing only the pixels that belong to good bins.
     """
-    bad_bins_index = np.array(range(bins_table.shape[0]))[bin_mask]
-    pixels_mask = chunk_pixels['bin1_id'].isin(bad_bins_index) + chunk_pixels['bin2_id'].isin(bad_bins_index)
+    good_bins_index = np.array(range(bins_table_size))[bins_mask]
+    pixels_mask = chunk_pixels['bin1_id'].isin(good_bins_index) * chunk_pixels['bin2_id'].isin(good_bins_index)
     return chunk_pixels[pixels_mask]
 
-def pixels_filter_generator(bins_table, bin_mask):
+def pixels_filter_generator(bins_table_size, bins_mask):
     """
     Returns a partial function that filters pixels based on the provided bins table and bin mask.
 
     Parameters:
     -----------
-    bins_table : dict
-        A dictionary containing the bins table.
-    bin_mask : numpy.ndarray
+    bins_table_size : int
+        A integer value indicates the size of bin table.
+    bins_mask : numpy.ndarray
         A numpy array containing the bin mask.
 
     Returns:
@@ -75,9 +80,9 @@ def pixels_filter_generator(bins_table, bin_mask):
         A partial function that filters pixels based on the provided bins table and bin mask.
     """
 
-    return functools.partial(_pixels_filter, bins_table=bins_table, bin_mask=bin_mask)
+    return functools.partial(_pixels_filter, bins_table_size=bins_table_size, bins_mask=bins_mask)
 
-class pixels_iterator:
+class PixelsIterator:
     def __init__(self, clr_pixels_selector, pixels_size, chunksize):
         self.chunksize = chunksize
         self.max = pixels_size
@@ -99,7 +104,7 @@ class pixels_iterator:
         else:
             raise StopIteration
 
-def filter_pixels_by_masked_bin(clr, thres, output_path, chunksize=10_000_000):
+def filter_pixels_by_masked_bin(clr, thres, output_path, bins_filters=None, nproc=16, chunksize=10_000_000):
     """
     Filter pixels of a cooler object based on a binary mask of genomic bins.
 
@@ -111,6 +116,10 @@ def filter_pixels_by_masked_bin(clr, thres, output_path, chunksize=10_000_000):
         A threshold value for filtering genomic bins based on their cis/trans ratio.
     output_path : str
         The path to the output cooler file.
+    bins_filters : list of functions, optional
+        A list of functions that generate binary masks for genomic bins. Default is None.
+    nproc : int, optional
+        The number of processes to use for parallelization. Default is 16.
     chunksize : int, optional
         The number of pixels to process at a time. Default is 10,000,000.
 
@@ -125,16 +134,22 @@ def filter_pixels_by_masked_bin(clr, thres, output_path, chunksize=10_000_000):
     pixels are written to a new cooler file at the specified output path.
 
     """
+    if bins_filters == None:
+        bins_filters = [_bins_cis_total_ratio_filter]
 
-    logger.debug('Start to make bin mask...')
-    bin_mask = _bins_cis_total_ratio_filter(clr, thres)
-    bins_table = clr.bins()[:]
-    tot_pixels = clr.pixels().shape[0]
+    for bins_filter in bins_filters:
+        logger.info(f'Start to make bin mask with {thres} threshold...')
+        bins_mask = bins_filter(clr, thres)
+        bins_table = clr.bins()[:]
+        tot_pixels = clr.pixels().shape[0]
 
-    logger.debug('Start to create pixels counts file...')
-    pixels_chunks = pixels_iterator(clr.pixels(), tot_pixels, chunksize)
-    pixels_filter = pixels_filter_generator(bins_table, bin_mask)
+        logger.debug('Start to create pixels counts file...')
+        pixels_chunks = PixelsIterator(clr.pixels(), tot_pixels, chunksize)
+        pixels_filter = pixels_filter_generator(bins_table.shape[0], bins_mask)
 
-    cooler.create_cooler(output_path, bins=bins_table, pixels=map(pixels_filter, pixels_chunks), ordered=True, columns=['count']) # input an iterator to pixels parameter
-
+        logger.debug('Start to create cooler file...')
+        p = Pool(nproc)
+        cooler.create_cooler(output_path, bins=bins_table, pixels=p.map(pixels_filter, pixels_chunks), ordered=True, columns=['count'])
+        p.close()
+        logger.debug('done')
     
--- test_func.py
+++ test_func.py
@@ -1,14 +1,19 @@
-import numpy as np
-import pandas as pd
+import sys
+sys.path.append('/home1/yxiao977/labwork/Akita_dinoflagellate')
+import data_util
 import cooler
 import logging
+import pandas as pd
+import cooltools
+import numpy as np
 
-logger = logging.getLogger('one')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
+logger = logging.getLogger('test_func')
 logger.setLevel(logging.DEBUG)
+logger.propagate = False  # Disable propagation to the root logger
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 # logger.disabled = True
@@ -16,70 +21,72 @@ logger.addHandler(ch)
 
 
 # create a small toy example inside the test func with a human readable small data
-def test_cis_total_filter(request):
-    '''
-    Test function for filtering out bad bins based on cis-total-ratio coverage.
+def test_cis_total_filter(thres, clr_test=None):
 
-    Parameters:
-    -----------
-    request : dict
-        A dictionary containing the paths to the Hi-C file and the cis-total-ratio coverage file.
+    if clr_test == None:
+        logger.debug('Start to generate a toy example...')
+        # Generate a toy example
+        bins=pd.DataFrame(
+            [["chr1", 0, 5],
+                ["chr1", 5, 8],
+                ["chrX", 0, 5],
+                ["chrX", 5, 10]],
+        columns=["chrom", "start", "end"],
+        )
 
-        Format:
-        request = {
-            'hic_path':'path',
-            'cis_total_path':'path'
-        }
+        # only has contacts within the same chromsomes
+        # only has contacts with the different chromosomes
+        # > 0.5 cis-total ratio
+        # = 0.5 cis-total ratio
+        pixels = pd.DataFrame(
+            [[0, 0, 6], 
+            [1, 2, 3], [1, 3, 35],
+            [2, 2, 3], [2, 3, 30],
+            [3, 3, 5]], 
+            columns=["bin1_id", "bin2_id", "count"]
+        )
 
-    Returns:
-    --------
-    None
+    clr_file = "/home1/yxiao977/sc1/train_akita/test_data/test_data_util.cool"
+    cooler.create_cooler(clr_file, bins, pixels)
+    clr_test = cooler.Cooler(clr_file)
+    cooler.balance_cooler(clr_test, ignore_diags=0, store=True, store_name='weight')
+    clr_test = cooler.Cooler(clr_file)
 
-    Raises:
-    -------
-    AssertionError
-        If the bad bins are not correctly filtered out.
 
-    Notes:
-    ------
-    This function reads the Hi-C file and the cis-total-ratio coverage file, and filters out the bins with a cis-total-ratio coverage lower than or equal to 0.5. It then checks if the bad bins are correctly filtered out by comparing the sum of the counts of the pixels that involve bad bins with NaN. The function raises an AssertionError if the bad bins are not correctly filtered out.
-    '''
+    logger.debug('Start to test filter_pixels function...')
+    output_path = f"/home1/yxiao977/sc1/train_akita/test_data/test_data_util_{thres}filtered.cool"
+    data_util.filter_pixels_by_masked_bin(clr_test, thres, output_path, bins_filters=None, nproc=16, chunksize=10_000_000)
+    clr_filtered = cooler.Cooler(output_path)
+
+    # check if clr_filtered has nan cis_total ratio for bins filtered out
+    logger.debug('Start to examine the result...')
+    coverage = cooltools.coverage(clr_test, ignore_diags=0)
+    cis_total_cov = coverage[0] / coverage[1]
+    bins_mask = cis_total_cov <= thres
+
+    coverage_filtered = cooltools.coverage(clr_filtered, ignore_diags=0)
+    cis_total_cov_filtered = coverage_filtered[0] / coverage_filtered[1]
+    cis_total_cov_filtered_bad = cis_total_cov_filtered[bins_mask]
+
+    assert np.isnan(cis_total_cov_filtered_bad).all()
     
-    # Read data
-    clr = cooler.Cooler(request['hic_path'])
-    cis_total_cov = pd.read_table(request['cis_total_path'], sep='\t')
-    cis_total_cov = cis_total_cov['cis-total-ratio'].to_numpy()
-
-    # Find bad bins
-    logger.debug('Start to find bad bins')
-    bad_bins_index = np.argwhere((cis_total_cov <= 0.5)).reshape(-1)
-    bad_bins = clr.bins()[:][clr.bins()[:].index.isin(bad_bins_index)]
-
-    #Find good bins
-    logger.debug('Start to find good bins')
-    good_bins_index = np.argwhere((cis_total_cov > 0.5)).reshape(-1)
-    good_bins = clr.bins()[:][clr.bins()[:].index.isin(good_bins_index)]
+    # check if good bins has nan cis-total ratio
+    logger.info('\n\n######### Pass the test #########')
 
 
-    # Check if bad bins are correctly filter out
-    logger.debug('Start to create pixel dataframe based on binid1')
-    bad_pixels_binid1 = [clr.pixels().fetch(bin.values[:3]) for index, bin in bad_bins.iterrows()]
-
-    logger.debug('Start to create pixel dataframe based on binid2')
-    good_pixels_dfs = [clr.pixels().fetch(bin.values[:3]) for index, bin in good_bins.iterrows()]
-    bad_pixels_binid2 = [df[df.bin2_id.isin(bad_bins_index)] for df in good_pixels_dfs]
-     
-    # 
-    logger.debug('Start to sum up counts')
-    check_nan = lambda x: np.isnan(x['count']).all()
-
-    check_result = np.sum(list(map(check_nan, bad_pixels_binid1)))
-    assert check_result == len(bad_pixels_binid1)
-    logger.info('Pass bin1 checkpoint')
+    
+    
+    
+    # logger.debug('Start to sum up counts')
+    # check_nan = lambda x: np.isnan(x['count']).all()
+    
+    # check_result = np.sum(list(map(check_nan, bad_pixels_binid1)))
+    # assert check_result == len(bad_pixels_binid1)
+    # logger.info('Pass bin1 checkpoint')
 
 
-    check_result = np.sum(list(map(check_nan, bad_pixels_binid2)))
-    assert check_result == len(bad_pixels_binid2)
-    logger.info('Pass bin2 checkpoint')
+    # check_result = np.sum(list(map(check_nan, bad_pixels_binid2)))
+    # assert check_result == len(bad_pixels_binid2)
+    # logger.info('Pass bin2 checkpoint')
 
 
