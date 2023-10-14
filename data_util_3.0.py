2461976451f0b66ed00dcfc13df7fa047b393d08
Yao Xiao <yxiao977@endeavour2.hpc.usc.edu>
2023-10-13 17:14:41 -0700

    v4

--- data_util_3.0.py
+++ data_util_3.0.py
@@ -4,8 +4,6 @@ import cooltools
 import cooler
 import functools
 from multiprocessing import Pool
-from inspect import signature
-
 
 import logging
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
@@ -17,23 +15,8 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
-
-def add_partial_thres_arguments(func):
-    def wrapper(*args, **kwargs):
-        if len(args) + len(kwargs) == 1:
-            if len(args) == 1:
-                thres_value = args[0]
-            else:
-                thres_value = kwargs['thres']
-
-            return functools.partial(func, thres=thres_value)
-        else:
-            return func(*args, **kwargs)
-    return wrapper
-
-
-@add_partial_thres_arguments
-def cis_total_ratio_filter(clr, thres=0.5):
+ 
+def cis_total_ratio_filter(clr, thres):
     """
     Filter out bins with low cis-to-total coverage ratio from a Cooler object.
 
@@ -55,7 +38,7 @@ def cis_total_ratio_filter(clr, thres=0.5):
 
     return bin_mask
 
-def generate_bin_mask(clr, filters=[None]):
+def generate_bin_mask(clr, filter_lst, thres_lst):
     """
     Generates a binary mask for a given `clr` object based on a list of filters and thresholds.
 
@@ -63,20 +46,22 @@ def generate_bin_mask(clr, filters=[None]):
     -----------
     clr : cooler.Cooler
         A cooler object containing Hi-C contact matrices.
-    filters : list
+    filter_lst : list
         A list of filter functions to apply to the contact matrices.
+    thres_lst : list
+        A list of thresholds to use for each filter function.
 
     Returns:
     --------
     bin_mask : numpy.ndarray
         A binary mask indicating which genomic bins pass all filters.
     """
-    if not isinstance(filters, list):
+    if not isinstance(filter_lst, list):
         logger.error('filter_lst parameter takes a list')
         
     bin_mask = np.array([True] * clr.bins().shape[0])
-    for filter in filters:
-        bin_mask *= filter(clr)
+    for i, filter in enumerate(filter_lst):
+        bin_mask *= filter(clr, thres_lst[i])
     
     return bin_mask
 
@@ -121,41 +106,40 @@ def pixel_iter_chunks(clr, chunksize):
         chunk = selector[lo:hi]
         yield chunk
 
-def pool_decorator(func):
+def pool_decorator(func, map_param, nproc):
+    """
+    A decorator function that parallelizes the execution of a given function using multiprocessing.Pool.
 
-    def wrapper(*args, **kwargs):
-        pool = None
-        if 'map' not in kwargs.keys() and len(args) <= len(signature(func).parameters) - 1:
-            if 'nproc' in kwargs.keys():
-                if kwargs['nproc'] <= 1:
-                    mymap = map
-                else:
-                    logger.debug('Start to use pool')
-                    pool = Pool(kwargs['nproc'])
-                    mymap = pool.map
-            elif len(args) == len(signature(func).parameters) - 1:
-                if args[len(signature(func).parameters) - 2] <= 1:
-                    mymap = map
-                else:
-                    logger.debug('Start to use pool')
-                    pool = Pool(args[len(signature(func).parameters) - 2])
-                    mymap = pool.map
-            else:
-                mymap = map
+    Parameters:
+    -----------
+    func : function
+        The function to be parallelized.
+    map_param : str
+        The name of the parameter in func that should be mapped over.
+    nproc : int
+        The number of processes to use for parallelization.
 
-            func(*args, **kwargs, map=mymap)
+    Returns
+    -------
+    decorated_func : function
+        The decorated function that parallelizes the execution of func.
+    """
+    def decorated_func(func_in_map, iter_chunks, *args, **kwargs):
 
+        if nproc > 1:
+            pool = Pool(nproc)
+            mymap = pool.map
         else:
+            mymap = map
 
-            func(*args, **kwargs)
+        func(*args, **kwargs, **{map_param: mymap(func_in_map, iter_chunks)})
         
-        if pool != None: 
+        if nproc > 1: 
             pool.close()
 
-    return wrapper
+    return decorated_func
 
-@pool_decorator
-def create_filtered_cooler(output_uri, clr, bin_mask, chunksize=10_000_000, nproc=1, map=map):
+def create_filtered_cooler(output_uri, clr, bin_mask, nproc=16, chunksize=10_000_000):
     """
     Create a filtered cooler file from a given cooler object and a binary mask of good bins.
 
@@ -177,16 +161,13 @@ def create_filtered_cooler(output_uri, clr, bin_mask, chunksize=10_000_000, npro
     -------
     None
     """
-    if len(bin_mask) != clr.bins().shape[0]:
-        raise ValueError('bin_mask should have the same length as bin table in cool file')
     logger.debug('Start to create cooler file...')
     bin_table = clr.bins()[:]
     good_bins_index = np.array(range(clr.bins().shape[0]))[bin_mask]
     pixels_filter = functools.partial(_pixel_filter, good_bins_index=good_bins_index)
 
-    cooler.create_cooler(output_uri, bins=bin_table, pixels=map(pixels_filter, pixel_iter_chunks(clr, chunksize)), ordered=True, columns=['count'])
+    pool_create_cooler = pool_decorator(cooler.create_cooler, map_param='pixels', nproc=nproc)
+    pool_create_cooler(pixels_filter, pixel_iter_chunks(clr, chunksize), output_uri, bins=bin_table, ordered=True, columns=['count'])
+
 
     logger.debug('done')
-
-
-# use the black
