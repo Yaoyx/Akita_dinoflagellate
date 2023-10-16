c193b9a74514b0fb7bbc5c30a32fbf73b9a69d3a
Yao Xiao <yxiao977@endeavour2.hpc.usc.edu>
2023-10-15 22:33:41 -0700

    simplify decorators

--- data_util.py
+++ data_util.py
@@ -4,31 +4,40 @@ import cooltools
 import cooler
 import functools
 from multiprocessing import Pool
-from inspect import signature
-
-
 import logging
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s')
-logger = logging.getLogger('data_util')
-logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s : %(message)s")
+logger = logging.getLogger("data_util")
 logger.propagate = False  # Disable propagation to the root logger
 
 ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def add_partial_thres_arguments(func):
-    def wrapper(*args, **kwargs):
-        if len(args) + len(kwargs) == 1:
-            if len(args) == 1:
-                thres_value = args[0]
-            else:
-                thres_value = kwargs['thres']
 
-            return functools.partial(func, thres=thres_value)
-        else:
-            return func(*args, **kwargs)
+def add_partial_thres_arguments(func):
+    """
+    Decorator that adds a threshold argument to a function and returns a partial function.
+
+    The returned partial function has the threshold argument set to the provided value.
+
+    Parameters
+    ----------
+    func : callable
+        The function to decorate.
+
+    Returns
+    -------
+    callable
+        A new function that takes a threshold argument and returns a partial function of `func`.
+    """
+
+    def wrapper(thres):
+        if not isinstance(thres, float):
+            raise ValueError("Threshold must be a float > 0 or <= 1.0")
+        return functools.partial(func, thres=thres)
+
     return wrapper
 
 
@@ -48,6 +57,10 @@ def cis_total_ratio_filter(clr, thres=0.5):
     -------
     numpy.ndarray
         An array of bin mask.
+
+    Note
+    ----
+    Generate a filter by giving a threshold
     """
     coverage = cooltools.coverage(clr)
     cis_total_cov = coverage[0] / coverage[1]
@@ -55,30 +68,32 @@ def cis_total_ratio_filter(clr, thres=0.5):
 
     return bin_mask
 
+
 def generate_bin_mask(clr, filters=[None]):
     """
     Generates a binary mask for a given `clr` object based on a list of filters and thresholds.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     clr : cooler.Cooler
         A cooler object containing Hi-C contact matrices.
     filters : list
         A list of filter functions to apply to the contact matrices.
 
-    Returns:
-    --------
+    Returns
+    -------
     bin_mask : numpy.ndarray
         A binary mask indicating which genomic bins pass all filters.
     """
     if not isinstance(filters, list):
-        logger.error('filter_lst parameter takes a list')
-        
+        logger.error("filter_lst parameter takes a list")
+
     bin_mask = np.array([True] * clr.bins().shape[0])
     for filter in filters:
         bin_mask *= filter(clr)
-    
+
     return bin_mask
+
 
 def _pixel_filter(chunk_pixels, good_bins_index):
     """
@@ -96,23 +111,26 @@ def _pixel_filter(chunk_pixels, good_bins_index):
     pandas.DataFrame
         A DataFrame containing only the pixels whose bin1_id and bin2_id are in good_bins_index.
     """
-    
-    pixels_mask = chunk_pixels['bin1_id'].isin(good_bins_index) * chunk_pixels['bin2_id'].isin(good_bins_index)
+
+    pixels_mask = chunk_pixels["bin1_id"].isin(good_bins_index) * chunk_pixels[
+        "bin2_id"
+    ].isin(good_bins_index)
     return chunk_pixels[pixels_mask]
+
 
 def pixel_iter_chunks(clr, chunksize):
     """
     Iterate over the pixels of a cooler object in chunks of a given size.
 
-    Parameters:
-    -----------
+    Parameters
+    ----------
     clr : cooler.Cooler
         A cooler object containing Hi-C data.
     chunksize : int
         The size of each chunk of pixels to iterate over.
 
-    Yields:
-    -------
+    Yields
+    ------
     chunk : numpy.ndarray
         A chunk of pixels of size `chunksize`.
     """
@@ -121,41 +139,48 @@ def pixel_iter_chunks(clr, chunksize):
         chunk = selector[lo:hi]
         yield chunk
 
+
 def pool_decorator(func):
+    """
+    A decorator function that enables multiprocessing for a given function.
+
+    Parameters
+    ----------
+    func : callable
+        The function to be decorated.
+
+    Returns
+    -------
+    A wrapper function that enables multiprocessing for the given function.
+    """
 
     def wrapper(*args, **kwargs):
         pool = None
-        if 'map' not in kwargs.keys() and len(args) <= len(signature(func).parameters) - 1:
-            if 'nproc' in kwargs.keys():
-                if kwargs['nproc'] <= 1:
-                    mymap = map
-                else:
-                    logger.debug('Start to use pool')
-                    pool = Pool(kwargs['nproc'])
-                    mymap = pool.map
-            elif len(args) == len(signature(func).parameters) - 1:
-                if args[len(signature(func).parameters) - 2] <= 1:
-                    mymap = map
-                else:
-                    logger.debug('Start to use pool')
-                    pool = Pool(args[len(signature(func).parameters) - 2])
-                    mymap = pool.map
+        if "nproc" in kwargs.keys():
+            if kwargs["nproc"] > 1:
+                logger.debug("Start to use pool")
+                pool = Pool(kwargs["nproc"])
+                mymap = pool.map
             else:
                 mymap = map
 
             func(*args, **kwargs, map=mymap)
 
         else:
+            raise TypeError(
+                "nproc must be specified as a keyword argument (e.g. nproc=1, nproc=5...)"
+            )
 
-            func(*args, **kwargs)
-        
-        if pool != None: 
+        if pool != None:
             pool.close()
 
     return wrapper
 
+
 @pool_decorator
-def create_filtered_cooler(output_uri, clr, bin_mask, chunksize=10_000_000, nproc=1, map=map):
+def create_filtered_cooler(
+    output_uri, clr, bin_mask, chunksize=10_000_000, nproc=1, map=map
+):
     """
     Create a filtered cooler file from a given cooler object and a binary mask of good bins.
 
@@ -178,15 +203,20 @@ def create_filtered_cooler(output_uri, clr, bin_mask, chunksize=10_000_000, npro
     None
     """
     if len(bin_mask) != clr.bins().shape[0]:
-        raise ValueError('bin_mask should have the same length as bin table in cool file')
-    logger.debug('Start to create cooler file...')
+        raise ValueError(
+            "bin_mask should have the same length as bin table in cool file"
+        )
+    logger.debug("Start to create cooler file...")
     bin_table = clr.bins()[:]
     good_bins_index = np.array(range(clr.bins().shape[0]))[bin_mask]
     pixels_filter = functools.partial(_pixel_filter, good_bins_index=good_bins_index)
 
-    cooler.create_cooler(output_uri, bins=bin_table, pixels=map(pixels_filter, pixel_iter_chunks(clr, chunksize)), ordered=True, columns=['count'])
+    cooler.create_cooler(
+        output_uri,
+        bins=bin_table,
+        pixels=map(pixels_filter, pixel_iter_chunks(clr, chunksize)),
+        ordered=True,
+        columns=["count"],
+    )
 
-    logger.debug('done')
-
-
-# use the black
+    logger.debug("done")
